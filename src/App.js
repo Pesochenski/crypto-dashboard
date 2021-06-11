d538014087be23af102910eeb73bb8b1a3ff9b13
Pesochenski <pesochenski.corp@gmail.com>
2021-06-11 05:12:01 +0300

    canvas template (#0)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React, { useEffect } from "react";
 import MainCanvas from "./components/mainCanvas";
 
 export default function App() {
-  // useEffect(() => {
-  //   gettingTestData();
-  // }, []);
+  useEffect(() => {
+    gettingTestData();
+  }, []);
 
-  // async function gettingTestData(): Promise<void> {
-  //   const res = await axios.get(
-  //     "https://api.binance.com/api/v1/klines?symbol=LTCBTC&interval=1d&limit=10"
-  //   );
-  //   console.log(res);
-  // }
+  async function gettingTestData() {
+    const res = await axios.get(
+      "https://api.binance.com/api/v1/klines?symbol=LTCBTC&interval=1d&limit=10"
+    );
+    console.log(res);
+  }
 
   return (
     <main>
--- src/components/mainCanvas/index.js
+++ src/components/mainCanvas/index.js
@@ -1,7 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./canvas.scss";
 
 export default function MainCanvas() {
+  const [min, setMin] = useState();
+  const [max, setMax] = useState();
+
+  function checkForMinAndMax(data) {
+    const Ydata = [];
+
+    for (const [, y] of data) {
+      Ydata.push(y);
+    }
+
+    for (let i = 0; i < Ydata.length; i++) {
+      let minIndex = i;
+      for (let j = i; j < Ydata.length; j++) {
+        if (Ydata[j] < Ydata[minIndex]) {
+          minIndex = j;
+        }
+        const stock = Ydata[i];
+        Ydata[i] = Ydata[minIndex];
+        Ydata[minIndex] = stock;
+      }
+    }
+
+    setMin(Ydata[0]);
+    setMax(Ydata[Ydata.length - 1]);
+    // return [Ydata[0], Ydata[Ydata.length - 1]];
+  }
+
+  const data = [
+    [0, 0],
+    [200, 200],
+    [400, 100],
+    [600, 400],
+    [800, 300],
+  ];
+
   const WIDTH = 600;
   const HEIGHT = 200;
   const PADDING = 40;
@@ -13,6 +48,7 @@ export default function MainCanvas() {
 
   const ROWS_COUNT = 5;
   const step = VEIW_HEIGHT / ROWS_COUNT;
+  const textStep = (max - min) / ROWS_COUNT;
 
   function charting(canvas, data) {
     const ctx = canvas.getContext("2d");
@@ -22,20 +58,23 @@ export default function MainCanvas() {
     canvas.width = DPI_WIDTH;
     canvas.height = DPI_HEIGHT;
 
+    // checkForMinAndMax(data);
+
     ctx.beginPath();
     ctx.strokeStyle = "#bbb";
-    ctx.lineWidth = 0.5;
+    // ctx.lineWidth = 0.5;
 
     ctx.font = "normal 20px Helvetica, sans-serif";
     ctx.fillStyle = "#96a2aa";
 
-    for (let i = 0; i <= ROWS_COUNT; i++) {
+    for (let i = 1; i <= ROWS_COUNT; i++) {
       const y = step * i;
-      ctx.fillText(String(DPI_HEIGHT - PADDING - y), 0, y + PADDING - 10);
+      const text = max - textStep * i;
+      ctx.fillText(text, 5, y + PADDING - 10);
       ctx.moveTo(0, y + PADDING);
       ctx.lineTo(DPI_WIDTH, y + PADDING);
-      ctx.stroke();
     }
+    ctx.stroke();
     ctx.closePath();
 
     ctx.beginPath();
@@ -43,20 +82,18 @@ export default function MainCanvas() {
     ctx.strokeStyle = "blue";
     for (const [x, y] of data) {
       ctx.lineTo(x, DPI_HEIGHT - PADDING - y);
-      ctx.stroke();
     }
-    ctx.closePath;
+    ctx.stroke();
+    ctx.closePath();
   }
 
   useEffect(() => {
-    charting(document.getElementById("main-canvas"), [
-      [0, 0],
-      [200, 100],
-      [400, 50],
-      [600, 400],
-      [800, 300],
-    ]);
+    checkForMinAndMax(data);
   }, []);
+
+  useEffect(() => {
+    charting(document.getElementById("main-canvas"), data);
+  }, [min]);
 
   return <canvas id="main-canvas" className="main-canvas" />;
 }
