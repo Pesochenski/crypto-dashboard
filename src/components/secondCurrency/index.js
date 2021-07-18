e80db553b6b62945d393dd15824caeb7cbacf6da
Pesochenski <pesochenski.corp@gmail.com>
2021-07-19 02:10:06 +0300

    adding choice btns

--- src/components/secondCurrency/index.js
+++ src/components/secondCurrency/index.js
@@ -1,17 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./second.scss";
 
 export default function SecondValueChange() {
   const [values, setValues] = useState(["USD", "BTC", "ETH", "LTC"]);
+  const [activeItem, setActiveItem] = useState(0);
+  useEffect(() => {
+    console.log(activeItem);
+  }, [activeItem]);
 
   return (
     <div className="value-choice">
       <input className="value-choice__input" type="text" placeholder="Add" />
       <div className="value-choice__btns">
         {values.map((item, i) => (
-          <button key={i} className="value-choice__item">
-            {item}
-          </button>
+          <div
+            key={i}
+            className="value-choice__item"
+            onMouseEnter={() => setActiveItem(i + 1)}
+            onMouseLeave={() => setActiveItem(0)}
+          >
+            <div className="value-choice__item-name">
+              <p
+                className={
+                  activeItem == i + 1
+                    ? "value-choice__item-text value-choice__item-text_hover"
+                    : "value-choice__item-text"
+                }
+              >
+                {item}
+              </p>
+            </div>
+            <button className="value-choice__item-del">
+              <svg className="value-choice__svg" viewBox="0 0 34 34">
+                <line
+                  x1="4"
+                  y1="22"
+                  x2="14"
+                  y2="12"
+                  className={
+                    activeItem == i + 1
+                      ? "value-choice__cross_hover"
+                      : "value-choice__cross"
+                  }
+                />
+                <line
+                  x1="14"
+                  y1="22"
+                  x2="4"
+                  y2="12"
+                  className={
+                    activeItem == i + 1
+                      ? "value-choice__cross_hover"
+                      : "value-choice__cross"
+                  }
+                />
+              </svg>
+            </button>
+          </div>
         ))}
       </div>
     </div>
