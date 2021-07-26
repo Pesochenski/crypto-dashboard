5b85df41af9fbb241f333261df59d043574c299b
Pesochenski <pesochenski.corp@gmail.com>
2021-07-26 03:04:55 +0300

    fix pagination with deleting cur

--- src/components/firstCurrency/index.js
+++ src/components/firstCurrency/index.js
@@ -11,12 +11,26 @@ export function FirstCurrencyChange() {
   ]);
   const [renderArr, setRenderArr] = useState([]);
   const [activePag, setActivePag] = useState(0);
+  const [hoverCur, setHoverCur] = useState(0);
+  const [activeCur, setActiveCur] = useState({
+    curName: firstValues[0],
+    curNum: 0,
+  });
 
   useEffect(() => {
     renderItems(0);
   }, []);
   useEffect(() => {
     renderItems(activePag);
+
+    if (!firstValues.includes(activeCur.curName)) {
+      setActiveCur({ curName: firstValues[0], curNum: 0 });
+    }
+
+    if (renderArr.length === 1 && activePag !== 0) {
+      // some react strange jokes... (=== 1)
+      renderItems(firstValues.length - 4);
+    }
   }, [firstValues]);
 
   function renderItems(num) {
@@ -32,7 +46,6 @@ export function FirstCurrencyChange() {
 
   function deleteAnyItem(item) {
     if (firstValues.length > 3 || (renderArr.length > 3 && activePag === 0)) {
-      // отрабатывает на любом паге хотя нужно только на 1
       setFirstValues(firstValues.filter((cur) => cur !== item));
       //   setRenderArr(renderArr.filter((cur) => cur !== item));
 
@@ -75,11 +88,41 @@ export function FirstCurrencyChange() {
       />
       <div className="currency-choice__values">
         {renderArr.map((item, i) => (
-          <div key={i} className="currency-choice__item">
+          <div
+            key={i}
+            className={
+              hoverCur === i + 1
+                ? "currency-choice__item currency-choice__item_hover"
+                : activeCur.curNum === firstValues.indexOf(item)
+                ? "currency-choice__item_active currency-choice__item"
+                : "currency-choice__item"
+            }
+            onMouseEnter={() => setHoverCur(i + 1)}
+            onMouseLeave={() => setHoverCur(0)}
+            onClick={() =>
+              setActiveCur({ curName: item, curNum: firstValues.indexOf(item) })
+            }
+          >
             <div className="currency-choice__item-header">
-              <p className="currency-choice__item-title">{item}</p>
+              <p
+                className={
+                  hoverCur === i + 1
+                    ? "currency-choice__item-title currency-choice__item-title_hover"
+                    : activeCur.curNum === firstValues.indexOf(item)
+                    ? "currency-choice__item-title_active currency-choice__item-title"
+                    : "currency-choice__item-title"
+                }
+              >
+                {item}
+              </p>
               <button
-                className="currency-choice__item-del"
+                className={
+                  hoverCur === i + 1
+                    ? "currency-choice__item-del currency-choice__item-del_hover"
+                    : activeCur.curNum === firstValues.indexOf(item)
+                    ? "currency-choice__item-del_active currency-choice__item-del"
+                    : "currency-choice__item-del"
+                }
                 onClick={() => deleteAnyItem(item)}
               >
                 Delete
